1ce471b68cc825bd72cc9d4e2d5b7bd5c9a0c9a4
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2023-11-01 11:14:30 +0900

    最大ソート回数を表示

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -113,6 +113,11 @@ export default function SearchPage(props: Props) {
             該当者: {members.length}名
           </Typography>
         </Grid>
+        <Grid container item xs={12} justifyContent="center" spacing={0}>
+          <Typography variant="h6" component="h2">
+            ソート回数: 〜{npDB.sort_count_estimate(members.length)}回
+          </Typography>
+        </Grid>
         <Grid>
           <FormGroup>
             <FormControlLabel control={<Checkbox checked={showHobby} onChange={(event) => {setShowHobby(event.target.checked)}} />} label="ソート時に趣味欄を表示する" />
