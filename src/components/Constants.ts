4bb787622ae8f61fb998bcd272008b6eda79459b
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2023-11-05 14:05:02 +0900

    ハッシュタグを修正

--- src/components/Constants.ts
+++ src/components/Constants.ts
@@ -1,4 +1,5 @@
 export const TITLE: string = "日プソート";
+export const HASHTAGS: string = "日プソート,日プ,日プ女子,PRODUCE101JAPANTHEGIRLS";
 export const VERSION: string = "1.0.0";
 export const IMAGE_DIR: string = "member_pics/";
 export const BOARDER: number = 50;
