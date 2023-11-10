96a47ef897dd31b6a79ec49bbdee5d74655b54d4
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2023-11-10 17:56:19 +0900

    SortPageのリファクタリング

--- src/pages/SortPage.tsx
+++ src/pages/SortPage.tsx
@@ -15,6 +15,7 @@ import Paper from '@material-ui/core/Paper';
 import { HASHTAGS, PAGE_URL } from "../modules/Constants";
 import { Member, SortSettings } from "../hooks/useNPDatabase";
 import { useLocation } from 'react-router-dom';
+import { useCallback } from "react";
 
 interface Props {
   members: Map<string, Member>;
@@ -28,14 +29,18 @@ export default function SortPage(props: Props) {
   const {members} = props;
 
   const sort = useRef<Sorter>();
-  const target_members = useRef<Map<string, Member>>(props.members);
+  const target_members = useRef<Map<string, Member>>(members);
+  const [result, setResult] = useState<boolean>();
+
   if (!sort.current) {
-    sort.current = new Sorter(Array.from(props.members.keys()));
+    // 初期化処理
+    sort.current = new Sorter(Array.from(members.keys()));
+    setResult(sort.current.sort());
   }
-  const [result, setResult] = useState(sort.current.sort());
 
   useEffect(() => {
     if (members.size !== target_members.current.size) {
+      target_members.current = members;
       sort.current = new Sorter(Array.from(members.keys()));
       setResult(sort.current.sort());
     }
@@ -59,8 +64,48 @@ function NowSortPage(props: {
   sort: Sorter;
   onSorted?: (result: boolean) => void;
 }) {
-  const [currentRound, setCurrentRound] = useState<number>(props.sort.currentRound);
+  const {sort, onSorted} = props;
+  const [currentRound, setCurrentRound] = useState<number>(sort.currentRound);
 
+  const leftWin = useCallback(() => {
+    sort.backable = true;
+    sort.prev_items = JSON.parse(JSON.stringify(sort.items));
+    sort.addResult(sort.lastChallenge[0], sort.lastChallenge[1]);
+    const result = sort.sort();
+    setCurrentRound(sort.currentRound);
+    onSorted?.(result);
+  }, [sort, onSorted]);
+
+  const rightWin = useCallback(() => {
+    sort.backable = true;
+    sort.prev_items = JSON.parse(JSON.stringify(sort.items));
+    sort.addResult(sort.lastChallenge[1], sort.lastChallenge[0]);
+    const result = sort.sort();
+    setCurrentRound(sort.currentRound);
+    onSorted?.(result);
+  }, [sort, onSorted]);
+
+  const bothWin = useCallback(() => {
+    sort.backable = true;
+    sort.prev_items = JSON.parse(JSON.stringify(sort.items));
+    sort.addEqual(sort.lastChallenge[0], sort.lastChallenge[1]);
+    const result = sort.sort();
+    setCurrentRound(sort.currentRound);
+    onSorted?.(result);
+  }, [sort, onSorted]);
+
+  const back = useCallback(() => {
+    if (sort.backable) {
+      sort.back();
+      const result = sort.sort();
+      setCurrentRound(sort.currentRound);
+      onSorted?.(result);
+    }
+    else {
+      alert("これ以上戻れません！")
+    }
+  }, [sort, onSorted]);
+  
   return (
     <div style={{ textAlign: "center" }}>
       <Grid container spacing={1}>
@@ -68,60 +113,27 @@ function NowSortPage(props: {
           <h2 style={{ marginBottom: 0 }}>{props.sortName}</h2>
         </Grid>
         <Grid container item xs={12} justifyContent="center">
-          <p style={{ marginTop: 0, marginBottom: 5 }}>ラウンド{currentRound} - {props.sort.progress}%</p>
+          <p style={{ marginTop: 0, marginBottom: 5 }}>ラウンド{currentRound} - {sort.progress}%</p>
         </Grid>
         <Grid container item xs={6} justifyContent="center">
-          <MemberPicture member={props.members.get(props.sort.lastChallenge[0])!}
+          <MemberPicture member={props.members.get(sort.lastChallenge[0])!}
             sortConfig={props.sortConfig}
-            onClick={() => {
-              props.sort.backable = true;
-              props.sort.prev_items = JSON.parse(JSON.stringify(props.sort.items));
-              props.sort.addResult(props.sort.lastChallenge[0], props.sort.lastChallenge[1]);
-              props.sort.sort();
-              setCurrentRound(props.sort.currentRound);
-            }} />
+            onClick={leftWin} />
         </Grid>
         <Grid container item xs={6} justifyContent="center">
-          <MemberPicture member={props.members.get(props.sort.lastChallenge[1])!}
+          <MemberPicture member={props.members.get(sort.lastChallenge[1])!}
             sortConfig={props.sortConfig}
-            onClick={() => {
-              props.sort.backable = true;
-              props.sort.prev_items = JSON.parse(JSON.stringify(props.sort.items));
-              props.sort.addResult(props.sort.lastChallenge[1], props.sort.lastChallenge[0]);
-              const result = props.sort.sort();
-              setCurrentRound(props.sort.currentRound);
-              props.onSorted?.(result);
-            }} />
+            onClick={rightWin} />
         </Grid>
         <Grid container item xs={12} justifyContent="center">
           <Button variant="contained" size="large" style={{ backgroundColor: "white", color: "#444" }}
-            onClick={() => {
-              props.sort.backable = true;
-              props.sort.prev_items = JSON.parse(JSON.stringify(props.sort.items));
-              props.sort.addEqual(props.sort.lastChallenge[0], props.sort.lastChallenge[1]);
-              props.sort.sort();
-              const result = props.sort.sort();
-              setCurrentRound(props.sort.currentRound);
-              props.onSorted?.(result);
-            }}
-          >
+            onClick={bothWin}>
             両方勝ち
           </Button>
         </Grid>
         <Grid container item xs={12} justifyContent="center">
           <Button size="large" style={{ backgroundColor: "#444", color: "white" }}
-            onClick={() => {
-              if (props.sort.backable) {
-                props.sort.back();
-                props.sort.sort();
-                const result = props.sort.sort();
-                setCurrentRound(props.sort.currentRound);
-                props.onSorted?.(result);
-              } else {
-                alert("これ以上戻れません！")
-              }
-            }}
-          >
+            onClick={back}>
             ひとつ戻る
           </Button>
         </Grid>
@@ -140,38 +152,49 @@ function SortResultPage(props: {
   sortName: string;
   sort: Sorter;
 }) {
-  let rankTable: JSX.Element[] = [];
-  let tweet_url: string = "https://twitter.com/intent/tweet?text=" + encodeURI(`${props.sortName}結果\n`);
-  let max_output = 11;
+  const {sort, sortName} = props;
 
-  let count = 1;
-  for (let i of props.sort.array) {
-    rankTable.push(<TableRow key={i}><TableCell align="left">{props.sort.rank(i)}位</TableCell><TableCell align="left">{i}</TableCell></TableRow>);
-    if (count <= max_output) {
-      tweet_url += encodeURI(`${props.sort.rank(i)}位: ${i}\n`);
-      count++;
+  const getRankTable = useCallback((): JSX.Element[] => {
+    const rankTable: JSX.Element[] = [];
+    for (let i of sort.array) {
+      rankTable.push(<TableRow key={i}><TableCell align="left">{sort.rank(i)}位</TableCell><TableCell align="left">{i}</TableCell></TableRow>);
     }
-  }
+    return rankTable;
+  }, [sort]);
 
-  const getResultPictures = (min: Number, max: Number) => {
+  const getTwitterIntentURL = useCallback((max_output: number): string => {
+    let tweet_url: string = "https://twitter.com/intent/tweet?text=" + encodeURI(`${sortName}結果\n`);
+
+    let count = 1;
+    for (let i of sort.array) {
+      if (count <= max_output) {
+        tweet_url += encodeURI(`${sort.rank(i)}位: ${i}\n`);
+        count++;
+      }
+    }
+    tweet_url += "&hashtags=" + encodeURI(HASHTAGS) + "&url=" + encodeURI(PAGE_URL);
+
+    return tweet_url;
+  }, [sort, sortName]);
+
+  const getResultPictures = useCallback((min: Number, max: Number) => {
     const result: JSX.Element[] = [];
-    for (let i of props.sort.array) {
-      if (props.sort.rank(i) >= min && props.sort.rank(i) <= max) {
+    for (let i of sort.array) {
+      if (sort.rank(i) >= min && sort.rank(i) <= max) {
         result.push(
-          <ResultPicture key={i} name={i} rank={props.sort.rank(i)}></ResultPicture>
+          <ResultPicture key={i} name={i} rank={sort.rank(i)}></ResultPicture>
         );
       }
     }
     return result;
-  }
-
-  tweet_url += "&hashtags=" + encodeURI(HASHTAGS) + "&url=" + encodeURI(PAGE_URL);
+  }, [sort]);
+  
   return (<Grid container alignItems="flex-start">
     <Grid container item xs={12} justifyContent="center">
       <h2 style={{ marginBottom: 0 }}>{props.sortName}結果</h2>
     </Grid>
     <Grid container item xs={12} justifyContent="center">
-      <p style={{ marginTop: 0, marginBottom: 10 }}>ラウンド{props.sort.currentRound} - {props.sort.progress}%</p>
+      <p style={{ marginTop: 0, marginBottom: 10 }}>ラウンド{sort.currentRound} - {sort.progress}%</p>
     </Grid>
     <Grid container item md={6} xs={12} justifyContent="center">
       <Grid container item xs={12} justifyContent="center">
@@ -198,7 +221,7 @@ function SortResultPage(props: {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rankTable}
+            {getRankTable()}
           </TableBody>
         </Table>
       </TableContainer>
@@ -206,7 +229,7 @@ function SortResultPage(props: {
     <Grid container item xs={12} justifyContent="center">
       <br />
       <p>
-        <Button href={tweet_url} target="_blank" variant="contained" size="large" style={{ backgroundColor: "#00ACEE", color: "#ffffff" }}>結果をツイート</Button>
+        <Button href={getTwitterIntentURL(11)} target="_blank" variant="contained" size="large" style={{ backgroundColor: "#00ACEE", color: "#ffffff" }}>結果をツイート</Button>
       </p>
     </Grid>
   </Grid>)
