3355e66a8155284ddacf0b1cdc6a0b6f88b528b3
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2023-11-14 13:47:50 +0900

    タイトルを変更

--- src/components/SearchConfig.tsx
+++ src/components/SearchConfig.tsx
@@ -1,20 +1,6 @@
-import Checkbox from "@material-ui/core/Checkbox/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
-import FormGroup from "@material-ui/core/FormGroup/FormGroup";
 import React from "react";
 import Typography from "@material-ui/core/Typography/Typography";
 import Button from "@material-ui/core/Button/Button";
-import { BOARDER } from "../modules/Constants";
-
-export const CanVoteCheckBox = React.memo((props: { canVote: boolean, setCanVote?: (canVote: boolean) => void }) => {
-  return (
-    <FormGroup>
-      <FormControlLabel id="checkbox-form-vote" control={<Checkbox checked={props.canVote} id="checkbox-vote" onChange={(event) => { props.setCanVote?.(event.target.checked) }} />} label={`投票対象(〜${BOARDER}位)のみ`} />
-    </FormGroup>
-  )
-}, (before, after) => {
-  return before.canVote === after.canVote
-})
 
 export const ResultText = React.memo((props: { count: number }) => {
   return (
@@ -34,39 +20,3 @@ export const SortStartButton = React.memo((props: { enabled: boolean, onClick?: 
     </Button>
   )
 });
-
-// const LabelCheckBox = React.memo((props: {checked: boolean, setChecked?: (canVote: boolean) => void, form_id: string, checkbox_id: string, label: string}) => {
-//   return (
-//     <FormGroup>
-//       <FormControlLabel id={props.form_id} control={<Checkbox checked={props.checked} id={props.checkbox_id} onChange={(event) => { props.setChecked?.(event.target.checked) }} />} label={props.label} />
-//     </FormGroup>
-//   )
-// }, (before, after) => {
-//   return before.checked === after.checked;
-// });
-
-// interface Props {
-//   onSortSettingsUpdated?: (setting: SortSettings) => void;
-// }
-
-// const SearchConfig = React.memo((props: Props) => {
-//   const [showHobby, setShowHobby] = useState<boolean>(false);
-//   const [showRanking, setShowRanking] = useState<boolean>(false);
-//   const [showSkill, setShowSkill] = useState<boolean>(false);
-
-//   useEffect(() => {
-//     props.onSortSettingsUpdated?.({ show_hobby: showHobby, show_skill: showSkill, show_ranking: showRanking })
-//     // eslint-disable-next-line
-//   }, [showHobby, showSkill, showRanking])
-
-//   return (
-//     <FormGroup>
-//       <LabelCheckBox checked={showHobby} setChecked={setShowHobby} form_id="checkbox-form-hobby" checkbox_id="checkbox-hobby" label="ソート時に趣味欄を表示する" />
-//       <LabelCheckBox checked={showSkill} setChecked={setShowSkill} form_id="checkbox-form-skill" checkbox_id="checkbox-skill" label="ソート時に特技欄を表示する" />
-//       <LabelCheckBox checked={showRanking} setChecked={setShowRanking} form_id="checkbox-form-ranking" checkbox_id="checkbox-rankings" label="ソート時に順位変動を表示する" />
-//     </FormGroup>
-//   );
-// }, (before, after) => {
-//   return before.onSortSettingsUpdated === after.onSortSettingsUpdated;
-// });
-// export default SearchConfig;
--- src/modules/Constants.ts
+++ src/modules/Constants.ts
@@ -1,41 +1,9 @@
-export const TITLE: string = "ハロプロソート";
-export const HASHTAGS: string = "ハロプロソート";
+export const TITLE: string = "ハロプロソート(3)";
+export const HASHTAGS: string = "ハロプロソート(3)";
 export const VERSION: string = "1.0.0";
 export const IMAGE_DIR: string = "member_pics/";
-export const BOARDER: number = 50;
 export const PAGE_URL: string = "https://np-sort.web.app/";
 export const SORT_PATH: string = "sort";
 export const NOW_LOADING: string = "読み込み中...";
 export const LATEST_CHANGE_LOG: string = "(最終更新:23/11/14 実験中)";
 export const MAXIMUM_TWEET_MEMBERS_COUNT = 10;
-
-export const PREF_SORT_MAP: Map<string, number> = new Map<string, number>([
-  ['北海道', 0],
-  ['岩手', 1],
-  ['宮城', 2],
-  ['福島', 3],
-  ['栃木', 4],
-  ['群馬', 5],
-  ['埼玉', 6],
-  ['千葉', 7],
-  ['東京', 8],
-  ['神奈川', 9],
-  ['新潟', 10],
-  ['石川', 11],
-  ['長野', 12],
-  ['愛知', 13],
-  ['三重', 14],
-  ['京都', 15],
-  ['大阪', 16],
-  ['兵庫', 17],
-  ['奈良', 18],
-  ['岡山', 19],
-  ['広島', 20],
-  ['徳島', 21],
-  ['香川', 22],
-  ['福岡', 23],
-  ['熊本', 24],
-  ['宮崎', 25],
-  ['アメリカ', 26],
-  ['インドネシア', 27]
-]);
