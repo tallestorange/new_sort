20beb8beb1c2652f05229ffc252ecf4e8fff265f
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2023-11-16 23:53:50 +0900

    tweetURLをconsole二表示

--- src/pages/SortPage.tsx
+++ src/pages/SortPage.tsx
@@ -219,6 +219,7 @@ function SortResultPage(props: {
       }
     }
     tweet_url += "&hashtags=" + encodeURI(HASHTAGS) + "&url=" + encodeURI(url);
+    console.log(tweet_url);
 
     return tweet_url;
   }, [sort, sortName, url]);
